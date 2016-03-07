10d6645761cb647d11e3420e32d1efb4bc53c4ff
Jaroslav Škarvada <jskarvad@redhat.com>
2016-03-07 22:14:16 +0100

    Added explicit casting to fix possible warning about signed/unsigned comparison
    Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

--- utils/directoryunpacker.cpp
+++ utils/directoryunpacker.cpp
@@ -424,8 +424,7 @@ void DirectoryUnpacker::processNetworkData()
 			{
 				qDebug() << "Unpacking MoNav Map Module:" << d->data->url().path();
 				// cast to qint64 to avoid a comparison between a signed and unsigned integers
-				qint64 header_length = strlen( "MoNav Map Module" );
-				if( d->data->bytesAvailable() < header_length )
+				if( d->data->bytesAvailable() < (qint64) strlen( "MoNav Map Module" ) )
 				{
 					//qDebug() << "Wating for header data";
 					return;
